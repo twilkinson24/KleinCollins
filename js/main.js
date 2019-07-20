07db36c452d8845a8fc86815a19187ebaeaf8c40
Taylor Wilkinson <taylorwlearns@gmail.com>
2019-07-20 17:30:45 -0500

    added mobile sidebar menu

---
+++ js/main.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function ($) {
+     // Toggle nav menu on button click
+     var $root = $("html, body"),
+          menu_toggle = $(".mobile-menu-toggle-btn");
+
+     menu_toggle.on("click", function () {
+          var $this = $(this);
+
+          $this.toggleClass("close");
+          $("#sidebar-menu, .menu-nav, .sidebar-nav-link").toggleClass("show");
+     });
+
+     // Toggle nav menu on item click and smooth scrolling
+     $(".menu-nav-item a").on("click", function () {
+          var $this = $(this),
+               href = $this.attr("href");
+
+          // Toggle nav menu
+          menu_toggle.removeClass("close");
+          $this.parents(".sidebar-nav-link, .menu-nav, #sidebar-menu").removeClass("show");
+
+          // Smooth scrolling
+          $root.animate(
+               {
+                    scrollTop: $(href).offset().top - 100
+               },
+               500,
+               function () {
+                    window.location.hash = href;
+               }
+          );
+          return false;
+     })
+});
