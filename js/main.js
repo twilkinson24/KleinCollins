c7add8b47da0ae0da61c797ab99fecde3ee17b4e
Taylor Wilkinson <taylorwlearns@gmail.com>
2019-07-23 15:06:14 -0500

    finished countdown timer

--- js/main.js
+++ js/main.js
@@ -91,4 +91,65 @@ jQuery(document).ready(function ($) {
                }
                this.previousTop = currentTop;
           });
+
+     if (!$("#klein-collins")[0]) {
+          console.log('yes');
+          (function () {
+               var _ = function (id) {
+                    return document.getElementById(id);
+               }
+               // Setup all the variables needed
+               var day = _('timer-days'),
+                    hour = _('timer-hours'),
+                    min = _('timer-minutes'),
+                    sec = _('timer-seconds'),
+                    // You can have as many date/times as you want in an array
+                    targets = [
+                         new Date('2019-09-25T16:30:00Z').getTime()],
+                    i = 0;
+
+               setInterval(function () {
+                    var now = new Date().getTime();
+                    var d = targets[i] - now;
+
+                    if (d > 0) {
+                         // Get number of weeks until event
+                         var weeks = Math.floor(d / 604800000);
+
+                         // Remove weeks from d variable and work out days
+                         d = d - (weeks * 604800000);
+                         var days = Math.floor(d / 86400000);
+                         day.innerHTML = days;
+
+                         // Remove days from d variable and work out hours
+                         d = d - (days * 86400000);
+                         var hours = Math.floor(d / 3600000);
+                         hour.innerHTML = hours;
+
+                         // Remove hours from d variable and work out minutes
+                         d = d - (hours * 3600000);
+                         var minutes = Math.floor(d / 60000);
+                         min.innerHTML = minutes;
+
+                         // Remove minutes from d variable and work out seconds
+                         d = d - (minutes * 60000);
+                         var seconds = Math.floor(d / 1000);
+                         sec.innerHTML = seconds;
+                    }
+
+                    if (d <= 0 && i < targets.length) {
+                         i++;
+                    }
+
+                    if (d <= 0 && i >= targets.length) {
+                         day.innerHTML = '0';
+                         hour.innerHTML = '0';
+                         min.innerHTML = '0';
+                         sec.innerHTML = '0';
+                    }
+
+               }, 1000);
+
+          }());
+     }
 });
