34e7d215d2274ed76b91507dd30460e6bdb5f5bc
Taylor Wilkinson <taylorwlearns@gmail.com>
2019-07-24 14:38:28 -0500

    added links and smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,4 +1,42 @@
 jQuery(document).ready(function ($) {
+     /** Smooth Scrolling */
+     // Select all links with hashes
+     $('a[href*="#"]')
+          // Remove links that don't actually link to anything
+          .not('[href="#"]')
+          .not('[href="#0"]')
+          .click(function (event) {
+               // On-page links
+               if (
+                    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                    &&
+                    location.hostname == this.hostname
+               ) {
+                    // Figure out element to scroll to
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    // Does a scroll target exist?
+                    if (target.length) {
+                         // Only prevent default if animation is actually gonna happen
+                         event.preventDefault();
+                         $('html, body').animate({
+                              scrollTop: target.offset().top
+                         }, 1000, function () {
+                              // Callback after animation
+                              // Must change focus!
+                              var $target = $(target);
+                              $target.focus();
+                              if ($target.is(":focus")) { // Checking if the target was focused
+                                   return false;
+                              } else {
+                                   $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                                   $target.focus(); // Set focus again
+                              };
+                         });
+                    }
+               }
+          });
+
      // Toggle nav menu on button click
      var $root = $("html, body"),
           menu_toggle = $(".mobile-menu-toggle-btn");
@@ -27,7 +65,6 @@ jQuery(document).ready(function ($) {
           $('header').removeClass("darken-header");
 
           // Smooth scrolling 
-
           $root.animate(
                {
                     scrollTop: $(href).offset().top - 100
